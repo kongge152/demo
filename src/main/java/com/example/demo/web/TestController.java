993c2fcf790bcd066088b2241904a9459077b7ea
wlin <1525488678@qq.com>
2019-03-13 13:55:22 +0800

    提交

---
+++ src/main/java/com/example/demo/web/TestController.java
@@ -0,0 +1,22 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wl on 2019/3/13.
+ */
+@RestController
+@RequestMapping("/demo")
+public class TestController {
+
+
+
+
+    @RequestMapping("/")
+    public  String test(){
+
+        return "Hello Word！！！";
+    }
+
+}
